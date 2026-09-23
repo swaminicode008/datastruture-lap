a941895c9f414cea22aad662a650b801157310fc
swaminicode008 <swamineebhagwat@gmail.com>
2026-09-23 13:42:42 +0530

    Fix variable names and improve student management system

--- student*menu.cpp
+++ student*menu.cpp
@@ -7,25 +7,59 @@ int student[10];
 int n=0;
 int choice;
 int marks;
-int search rollNo;
+int searchRollNo;
 
 do
 {
 cout<<"\n\n====Student Managment System====";
 cout<<"\n1.Add student";
 cout<<"\n2.Display student";
-cout<<"\n3.Search student
-";
+cout<<"\n3.Search student";
 cout<<"\n4.Exit";
 cout<<"\nEnter your choice:";
 cin>> choice;
 if(choice==1)
 {
-cout<<"Enter student rollNo :";
-cin>>student rollNo[n];
-n++
-cout<<"student rollNo Added!";
+cout<<"Enter student RollNo :";
+cin>>student RollNo[n];
+n++;
+cout<<"student RollNo Added!";
 }
 else if(choice==2)
 {
-cout<<"\n
+cout<<"\n student marks:\n";
+for(int i=0;i<n;i++)
+{
+cout<<students marks[i]<<endl;
+}
+}
+else if (choice==3)
+{
+cout<<"Enter student RollNo to search:";
+cin>>searchRollNo;
+bool found=false;
+for(int i=0;i<n;i++)
+{
+if (student[i]==searchRollNo)
+{
+found=true;
+}
+}
+if(found)
+{
+cout<<"student found !";
+}
+else
+{
+cout<<"student not found !";
+{
+{
+else if(choice==4)
+{
+cout<<"Thank you!";
+}
+else
+{
+cout<<"Invalid choice !";
+}
+}
