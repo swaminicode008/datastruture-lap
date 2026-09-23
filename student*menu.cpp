2f5fc310ec02a723d2e39ef39ce8541d817a01e5
swaminicode008 <swamineebhagwat@gmail.com>
2026-09-23 12:26:27 +0530

    Create student*menu.cpp

---
+++ student*menu.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int student[10];
+int n=0;
+int choice;
+int marks;
+int search rollNo;
+
+do
+{
+cout<<"\n\n====Student Managment System====";
+cout<<"\n1.Add student";
+cout<<"\n2.Display student";
+cout<<"\n3.Search student
+";
+cout<<"\n4.Exit";
+cout<<"\nEnter your choice:";
+cin>> choice;
+if(choice==1)
+{
+cout<<"Enter student rollNo :";
+cin>>student rollNo[n];
+n++
+cout<<"student rollNo Added!";
+}
+else if(choice==2)
+{
+cout<<"\n
